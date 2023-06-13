2c2b2cdd1b696ee5c1ba5dbbb2ebffa781d4168f
Vortal13 <100987@bk.ru>
2023-06-13 19:15:40 +0300

    Домашка 1

---
+++ DZ1_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+System.Console.Write("Введите первое число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите третье число: ");
+int numC = Convert.ToInt32(Console.ReadLine());
+
+if (numA > numB && numA > numC)
+    System.Console.WriteLine($"Наибольшее число: {numA}");
+if (numB > numA && numB > numC)
+    System.Console.WriteLine($"Наибольшее число: {numB}");
+if (numC > numB && numC > numA)
+    System.Console.WriteLine($"Наибольшее число: {numC}");
---
+++ DZ1_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+System.Console.Write("Введите число для проверки: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{ System.Console.WriteLine($"Число {number} чётное"); }
+else
+{ System.Console.WriteLine($"Число {number} нечётное"); }
