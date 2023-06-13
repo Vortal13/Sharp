ecf43fca794973afb775dd6c7e909d6d422fdc27
Vortal13 <100987@bk.ru>
2023-06-13 19:11:13 +0300

    Семинар 1

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 ->  "-2, -1, 0, 1, 2"
+
+System.Console.Write("Введите число: ");
+int N = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int count = 0;
+while (count <= 2*N)
+{
+    System.Console.Write($"{-N + count}, ");
+    count++;
+}
---
+++ task4/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая  принимает  на вход трехзначное и на выходе показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+System.Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int result = N % 10;
+System.Console.WriteLine($"{N} -> {result}");
