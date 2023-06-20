0d0ca89979660ee7f2b74217f07523b7028b60ab
Vortal13 <100987@bk.ru>
2023-06-20 21:31:57 +0300

    Урок 4. Функции

---
+++ task15_seminar_4/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+// Console.WriteLine("Введите число: ");
+// int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+// string num = number.ToString();
+// System.Console.WriteLine(num.Length);
+
+//  Метод 1
+// System.Console.WriteLine("Введите целое число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int DigitCapacity(int num)
+// {
+//     int digitCapacity = 0;
+//     int buff = 0;
+//     if (num == 0)
+//     {
+//         digitCapacity = 1;        
+//     return digitCapacity;   
+//     }    
+//     else
+//    {
+//     for (int i = 1; buff != num; i *= 10)
+//     {
+//     buff = buff + i * ((num / i) % 10);
+//      digitCapacity++;
+//     }
+//     return digitCapacity;
+//     }
+// }
+// System.Console.WriteLine($"Количество разрядов числа {number} - {DigitCapacity(number)}");
+
+// Метод 2
+
+int CountingDigit(int num)
+{
+    int count = 0;
+    while (num > 0)
+    {
+        num /= 10;
+        count++;
+    }
+    return count;
+}
+
+Console.Write("Введите число: ");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+if (CountingDigit(number) == 0)
+{
+    System.Console.WriteLine("1");
+}
+else
+{
+    System.Console.WriteLine(CountingDigit(number));
+}
---
+++ task17_seminar_4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int[] array = new int[8];
+// вводим новый массив из 8 эл-ов
+Random rnd = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(0, 2);
+    // System.Console.Write($"{array[i]} ");
+}
+
+System.Console.WriteLine($"[{string.Join(", ", array)}]");
