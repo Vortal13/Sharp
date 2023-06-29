2f89781b7bd983dc0f42ee009f46fabc03ec673b
Vortal13 <100987@bk.ru>
2023-06-29 18:38:53 +0300

    Домашние задачи по теме: Урок 6. Одномерные массивы

---
+++ DZ6_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+System.Console.Write("Введите размер массива: ");
+int numbers = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+int[] array = new int[numbers];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    System.Console.Write($"Введите значение {i}: ");
+    array[i]=Convert.ToInt32(Console.ReadLine());
+    
+    if (array[i] > 0)
+        count++;
+    }
+
+System.Console.WriteLine($"Колличество введённых чисел больше нуля равно: {count}");
