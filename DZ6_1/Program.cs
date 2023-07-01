cc905157008bc55bc8913abced5670bb9740cf67
Vortal13 <100987@bk.ru>
2023-07-01 11:57:44 +0300

    Урок 7. Двумерные массивы

--- DZ6_1/Program.cs
+++ DZ6_1/Program.cs
@@ -2,18 +2,65 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-System.Console.Write("Введите размер массива: ");
-int numbers = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+// System.Console.Write("Введите размер массива: ");
+// int numbers = Math.Abs(Convert.ToInt32(Console.ReadLine()));
 
-int[] array = new int[numbers];
-int count = 0;
-for (int i = 0; i < array.Length; i++)
+// int[] array = new int[numbers];
+// int count = 0;
+// for (int i = 0; i < array.Length; i++)
+// {
+//     System.Console.Write($"Введите значение {i}: ");
+//     array[i] = Convert.ToInt32(Console.ReadLine());
+
+//     if (array[i] > 0)
+//         count++;
+// }
+
+// System.Console.WriteLine($"Колличество введённых чисел больше нуля равно: {count}");
+
+
+// Пример идеального решения
+int Prompt(string message)   // считать число с консоли
 {
-    System.Console.Write($"Введите значение {i}: ");
-    array[i]=Convert.ToInt32(Console.ReadLine());
-    
-    if (array[i] > 0)
-        count++;
-    }
+    System.Console.Write(message);  // ввывести  сообщение
+    string value = Console.ReadLine(); //считывает с консоли строку
+    int result = Convert.ToInt32(value); // преобразование строки в целое число
+    return result;
+}
 
-System.Console.WriteLine($"Колличество введённых чисел больше нуля равно: {count}");
+int[] InputArray(int lenght) // ввод массива
+{
+    int[] array = new int[lenght];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Prompt($"Введите {i + 1}-й элемент: ");
+    }
+    return array;
+}
+
+void PrintArray(int[] array) // вывод массива
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.WriteLine($"a[{i}] = {array[i]}");
+    }
+}
+
+int CountPositivNumbers(int[] array) //перебор значений в массиве на условие
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+return count;
+}
+
+int lenght = Prompt("Введите количество элементов: ");
+int[] array;
+array = InputArray(lenght);
+PrintArray(array);
+System.Console.WriteLine($"Колличество введённых чисел больше нуля равно: {CountPositivNumbers(array)}");
---
+++ task30_seminar_7/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+
+
+void FillMatrixWithRandom(int[,] matrix)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(0, 10);
+        }
+    }
+}
+int[,] CheckOutChetnost (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+               matrix[i, j] = matrix[i,j] * matrix[i,j];
+            }
+        }
+    }
+return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+System.Console.Write("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[row, column];
+FillMatrixWithRandom(matrix);
+PrintMatrix(matrix);
+CheckOutChetnost(matrix);
+System.Console.WriteLine();
+PrintMatrix(matrix);
